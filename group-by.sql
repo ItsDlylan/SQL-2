0903a1fa1bf8ba2426048f7c5651152fd53173d5
Dylan Schwindt <dfieldmark69@gmail.com>
2021-08-12 15:12:42 -0700

    Basic Lab Done

---
+++ group-by.sql
@@ -0,0 +1,19 @@
+-- Copy, paste, and run the SQL code from the summary.
+
+-- Delete all ‘lion’ entries from the table.
+
+-- Delete all animals whose names start with “M”.
+
+-- Delete all entries whose age is less than 9.
+
+
+
+
+
+-- INTERMEDIATE GROUP BY 
+
+Find a count of how many tracks there are per genre. Display the genre name with the count.
+
+Find a count of how many tracks are the “Pop” genre and how many tracks are the “Rock” genre.
+
+Find a list of all artists and how many albums they have.
