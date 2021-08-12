7b887a2d66abd943670429595716e04f832bc127
Dylan Schwindt <dfieldmark69@gmail.com>
2021-08-12 15:32:14 -0700

    Alter-Table Complete.

--- alter-table.sql
+++ alter-table.sql
@@ -1,9 +1,15 @@
--- Copy, paste, and run the insert statement from the delete section above. (You should have deleted all the entries.)
 
--- Add a column to the animals table called “location”. It should have a VARCHAR data type.
-
+-- Add a column to the animals table called “location”. 
+-- It should have a VARCHAR data type.
+ALTER TABLE animals 
+ADD Location VARCHAR(30);
 -- Insert 3 new animals into the table – make sure none of their fields are null.
+INSERT INTO animals ( name, type, age, location ) 
+VALUES ('Leo1', 'lion', 12, 'Africa'), ('Leo2', 'lion', 12, 'Africa'), ('Leo3', 'lion', 12, 'Africa');        
 
 -- Change the “type” column’s name to be “species”.
-
+ALTER TABLE animals
+RENAME COLUMN type TO species;
 -- Change the “species” column data type to be VARCHAR.
+ALTER TABLE animals
+ALTER COLUMN species type VARCHAR(255);
