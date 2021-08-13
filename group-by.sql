14c1e1b4e6d1856a402282ce7c2bbb83e62e4edd
Dylan Schwindt <dfieldmark69@gmail.com>
2021-08-12 18:11:42 -0700

    Finished Group By

--- group-by.sql
+++ group-by.sql
@@ -1,19 +1,24 @@
--- Copy, paste, and run the SQL code from the summary.
 
--- Delete all ‘lion’ entries from the table.
-
--- Delete all animals whose names start with “M”.
-
--- Delete all entries whose age is less than 9.
-
-
+-- Find a count of how many tracks there are per genre. Display the genre name with the count.
+SELECT COUNT(*), g.name "Genre"
+FROM track t
+JOIN genre g 
+ON t.genre_id = g.genre_id
+GROUP BY g.name;
 
 
+-- Find a count of how many tracks are the “Pop” genre and how many tracks are the “Rock” genre.
+SELECT COUNT(*) "number of songs", g.name "Genre"
+FROM track t 
+JOIN genre g 
+ON t.genre_id = g.genre_id
+WHERE g.name = 'Pop' OR g.name = 'Rock'
+GROUP BY g.name;
 
--- INTERMEDIATE GROUP BY 
+-- Find a list of all artists and how many albums they have.
 
-Find a count of how many tracks there are per genre. Display the genre name with the count.
-
-Find a count of how many tracks are the “Pop” genre and how many tracks are the “Rock” genre.
-
-Find a list of all artists and how many albums they have.
+SELECT COUNT(*), ar.name "Artist"
+FROM artist ar
+JOIN album al
+ON ar.artist_id = al.artist_id
+GROUP BY ar.name;
